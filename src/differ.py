2b8fbe68ab88fd179871bbd04065d2e13136b255
Denny Bucklin <dennis.bucklin@gmail.com>
2024-05-25 10:03:10 -0500

    updated endpoint diff-pdf

--- src/differ.py
+++ src/differ.py
@@ -7,8 +7,14 @@ GREEN = '\x1b[38;5;2m'
 BLUE = '\x1b[38;5;4m'
 
 
-def extract_text_from_pdf(pdf_path):
-    document = fitz.open(pdf_path)
+def open_pdf(pdf_path=None, pdf_stream=None):
+    if pdf_path:
+        return fitz.open(pdf_path)
+    elif pdf_stream:
+        return fitz.open(stream=pdf_stream)
+
+
+def extract_text_from_pdf(document):
     text = ""
     for page_num in range(document.page_count):
         text += f"Page number: {page_num}\n"
@@ -21,20 +27,15 @@ def compare_texts(text1, text2):
     diff = difflib.ndiff(
         text1.splitlines(),
         text2.splitlines(),
-        # lineterm=''
     )
     return list(diff)
 
 
-def leftpad(line):
+def leftpad(line_num):
     spaces = 4
-    num = line
-    while spaces > 0:
-        if num // 10 == 0:
-            break
-        spaces -= 1
-        num //= 10
-    return " " * spaces + str(line)
+    line_num = str(line_num)
+    spaces -= len(line_num)
+    return " " * spaces + line_num
 
 
 def summarize_changes(diff):
@@ -44,22 +45,31 @@ def summarize_changes(diff):
         count += 1
         if line[:1] in "-":
             summary.append(
-                RED + line[:1] + leftpad(count) + f":{line[1:]}" + RESET
+                f"{RED}{line[:1]}{leftpad(count)}:{line[1:]}{RESET}"
             )
         elif line[:1] == "+":
             summary.append(
-                GREEN + line[:1] + leftpad(count) + f":{line[1:]}" + RESET
+                f"{GREEN}{line[:1]}{leftpad(count)}:{line[1:]}{RESET}"
             )
         else:
             summary.append(
-                BLUE + " " + leftpad(count) + f":{line[1:]}" + RESET
+                f"{BLUE} {leftpad(count)}:{line[1:]}{RESET}"
             )
     return summary
 
 
-def compare_pdfs(pdf1_path, pdf2_path):
-    pdf1_text = extract_text_from_pdf(pdf1_path)
-    pdf2_text = extract_text_from_pdf(pdf2_path)
+def compare_pdfs(pdf1_input, pdf2_input):
+    if isinstance(pdf1_input, str):
+        document1 = open_pdf(pdf1_input)
+    else:
+        document1 = open_pdf(pdf_stream=pdf1_input)
+    if isinstance(pdf2_input, str):
+        document2 = open_pdf(pdf2_input)
+    else:
+        document2 = open_pdf(pdf_stream=pdf2_input)
+
+    pdf1_text = extract_text_from_pdf(document1)
+    pdf2_text = extract_text_from_pdf(document2)
 
     diff = compare_texts(pdf1_text, pdf2_text)
     full_summary = summarize_changes(diff)
@@ -67,8 +77,8 @@ def compare_pdfs(pdf1_path, pdf2_path):
 
 
 if __name__ == "__main__":
-    pdf1_path = 'test 4350.pdf'
-    pdf2_path = 'test 5350.pdf'
+    pdf1_path = 'doc1.pdf'
+    pdf2_path = 'doc2.pdf'
     summary = compare_pdfs(pdf1_path, pdf2_path)
     for line in summary:
         print(line)
